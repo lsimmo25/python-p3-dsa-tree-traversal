f07e51ab0d2fbfac09b2828a8606a65bfc45f953
Lyle Simmons <lylesimmons125@gmail.com>
2024-05-03 20:20:50 -0400

    lab complete

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,20 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if not self.root:
+            return None
+
+        return self._find_element_by_id(self.root, id)
+
+    def _find_element_by_id(self, current, id):
+        if current.get("id") == id:
+            return current
+
+        for child in current.get("children", []):
+            result = self._find_element_by_id(child, id)
+            if result:
+                return result
+        return None
+      
